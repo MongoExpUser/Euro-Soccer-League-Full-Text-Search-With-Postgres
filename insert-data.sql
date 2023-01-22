604883808b863634a7a9c39b89cf2d09992f5286
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2023-01-21 22:38:39 -0600

    Create insert-data.sql

---
+++ insert-data.sql
@@ -0,0 +1,31 @@
+-- Insert rows of data
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Manchester United', 'Premier League', 20, 5, 3);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Barcelona', 'La Liga', 18, 7, 5);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Bayern Munich', 'Bundesliga', 22, 2, 4);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Juventus', 'Serie A', 19, 5, 4);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Paris Saint-Germain', 'Ligue 1', 21, 4, 3);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Liverpool', 'Premier League', 18, 7, 5);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Real Madrid', 'La Liga', 17, 8, 5);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Atletico Madrid', 'La Liga', 16, 9, 5);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Inter Milan', 'Serie A', 18, 7, 5);
+
+INSERT INTO soccer (team_name, league, wins, losses, draws)
+VALUES ('Chelsea', 'Premier League', 17, 8, 5);
