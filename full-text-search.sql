ed3b4f6f2a49f1f0739be363f4670075764a1769
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2023-01-22 15:28:35 -0600

    Update full-text-search.sql

--- full-text-search.sql
+++ full-text-search.sql
@@ -1,17 +1,17 @@
 
--- perform a full-text searches on the "team_name" and "league" columns using the @@ operator,
+-- perform full-text searches on the "team_name" and "league" columns using the @@ operator and the to_tsquery function
 SELECT team_name FROM soccer WHERE search_vector_team_name @@ to_tsquery('united');
 SELECT * FROM soccer WHERE search_vector_team_name @@ to_tsquery('united');
 SELECT team_name FROM soccer WHERE search_vector_league @@ to_tsquery('premier');
 SELECT * FROM soccer WHERE search_vector_league @@ to_tsquery('premier');
 
--- use the plainto_tsquery function to search for a phrase and the && and || operators to search for multiple words:
+-- use the @@ operator with the plainto_tsquery function to search for a phrase and the && and || operators to search for multiple words:
 SELECT team_name FROM soccer WHERE search_vector_team_name @@ plainto_tsquery('manchester united');
 SELECT * FROM soccer WHERE search_vector_team_name @@ plainto_tsquery('liverpool', );
 SELECT team_name FROM soccer WHERE search_vector_league @@ plainto_tsquery('premier league');
 SELECT *  FROM soccer WHERE search_vector_league @@ plainto_tsquery('premier league');
 
--- perform more complicate seach with @@, ||, AND and OR operators, and to_tsquery and plainto_tsquery functions
+-- perform more complicate seach with the @@, ||, AND and OR operators, and the to_tsquery and plainto_tsquery functions
 SELECT team_name FROM soccer WHERE search_vector_team_name @@ (to_tsquery('manchester') && to_tsquery('liver'));
 SELECT * FROM soccer WHERE search_vector_team_name @@ ( to_tsquery('manchester') || to_tsquery('liver') );
 SELECT * FROM soccer WHERE search_vector_team_name @@ ( to_tsquery('manchester') || to_tsquery('premier') );
