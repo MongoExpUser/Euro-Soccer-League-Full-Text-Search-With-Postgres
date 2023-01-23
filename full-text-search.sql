58fb6268ccb987b9e845974b7241ffbe44a24117
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2023-01-22 19:34:05 -0600

    Update full-text-search.sql

--- full-text-search.sql
+++ full-text-search.sql
@@ -5,13 +5,14 @@ SELECT * FROM soccer WHERE search_vector_team_name @@ to_tsquery('united');
 SELECT team_name FROM soccer WHERE search_vector_league @@ to_tsquery('premier');
 SELECT * FROM soccer WHERE search_vector_league @@ to_tsquery('premier');
 
-
+-- use the @@ operator and the plain_to_tsquery function to search for a phrase
 SELECT team_name FROM soccer WHERE search_vector_team_name @@ plainto_tsquery('manchester united');
 SELECT * FROM soccer WHERE search_vector_team_name @@ plainto_tsquery('liverpool', );
 SELECT team_name FROM soccer WHERE search_vector_league @@ plainto_tsquery('premier league');
 SELECT *  FROM soccer WHERE search_vector_league @@ plainto_tsquery('premier league');
 
--- perAND and OR operators, and the to_tsquery and plainto_tsquery functions
+-- use the @@ operator and the to_tsquery and plain_to_tsquery functions, to search for a word and a phrase, respetcively
+-- and then combine both with the && and || operators to search for multiple words amd phrases:
 SELECT team_name FROM soccer WHERE search_vector_team_name @@ (to_tsquery('manchester') && to_tsquery('liver'));
 SELECT * FROM soccer WHERE search_vector_team_name @@ ( to_tsquery('manchester') || to_tsquery('liver') );
 SELECT * FROM soccer WHERE search_vector_team_name @@ ( to_tsquery('manchester') || to_tsquery('premier') );
@@ -19,7 +20,6 @@ SELECT * FROM soccer WHERE search_vector_team_name @@ ( to_tsquery('manchester')
 
 
 -- perform more complicate seach with the @@, ||, AND and OR operators, and the to_tsquery and plainto_tsquery functions
-
 SELECT * FROM soccer
   WHERE search_vector_team_name @@ ( to_tsquery('manchester') )
   AND search_vector_league @@ ( plainto_tsquery('premier league') );
