75d19a2db3c43ad3a52bfd84230246e0bb4e41ce
MongoExpUser <30372796+MongoExpUser@users.noreply.github.com>
2023-01-24 12:42:26 -0600

    Update login-create-db-and-schema.sql

--- login-create-db-and-schema.sql
+++ login-create-db-and-schema.sql
@@ -1,7 +1,7 @@
 -- login, create database, create schema, and set path
 -- all command=s form postgres shell
 
--- login on Ubuntu : udo -u postgres psql -d postgres
+-- login on Ubuntu : sudo -u postgres psql -d postgres
 -- login on MacOS  : psql -d postgres
 
 CREATE DATABASE euro WITH TEMPLATE=template0 OWNER=postgres ENCODING='UTF8' LOCALE='C';
